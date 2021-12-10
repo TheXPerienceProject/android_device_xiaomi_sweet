c28f5662fb8a387a65d1661ec84f27d11ecb1182
klozz <klozz@thexperienceproject.org>
2021-12-10 12:21:25 +0100

    sweet: Drop telephony packages they are defined in vendor/xperience
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- device.mk
+++ device.mk
@@ -155,17 +155,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext \
-
 PRODUCT_BOOT_JARS += \
+    tcmiface \
     telephony-ext
 
 # FM
