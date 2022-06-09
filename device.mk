6c442561b855cb6e1def8249c277df0c566f8c19
klozz <carlosj@klozz.dev>
2022-06-09 06:57:42 +0200

    sweet: Remove BOOTJAR duplications
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -159,10 +159,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-PRODUCT_BOOT_JARS += \
-    tcmiface \
-    telephony-ext
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
