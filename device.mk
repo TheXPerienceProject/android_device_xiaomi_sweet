16e06c68b5f11ac7216a28c52b204cbbd2509db9
klozz <carlosj@klozz.dev>
2022-06-10 02:15:03 +0200

    sweet: build GrapheneOS camera
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -187,6 +187,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    Camera \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
